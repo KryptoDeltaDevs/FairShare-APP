8d03a09bb71fcdc3a9a456e1d551166267a7db7b
Tedious8 <tadeus.tamus@gmail.com>
2025-05-04 23:33:59 +0800

    initial commit

---
+++ app/controllers/app.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'slim'
+
+module FairShare
+  # Base class for FairShare Web Application
+  class App < Roda
+  end
+end
---
+++ config/environments.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'roda'
+require 'figaro'
+require 'logger'
+require 'rack/session'
+
+module FairShare
+  # Configuration for the API
+  class App < Roda
+    plugin :environments
+
+    Figaro.application = Figaro::Application.new(
+      environment:,
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+    def self.config = Figaro.env
+
+    # Session configuration
+    ONE_WEEK = 7 * 24 * 60 * 60
+    use Rack::Session::Cookie,
+        expire_after: ONE_WEEK,
+        secret: config.SESSION_SECRET
+
+    # HTTP Request logging
+    configure :development, :production do
+      plugin :common_logger, $stdout
+    end
+
+    # Custom events logging
+    LOGGER = Logger.new($stderr)
+    def self.logger = LOGGER
+
+    configure :development, :test do
+      logger.level = Logger::ERROR
+    end
+
+    # Console/Pry configuration
+    configure :development, :test do
+      require 'pry'
+
+      # Allows running reload! in pry to restart entire app
+      def self.reload!
+        exec 'pry -r ./spec/test_load_all'
+      end
+    end
+  end
+end
