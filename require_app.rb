c522faf55233b8fbcaf8d7d290fc345faf6199e0
Mercy1303 <mercy89313@gmail.com>
2025-05-07 19:37:09 +0800

    add logo and fix bugs

--- require_app.rb
+++ require_app.rb
@@ -3,7 +3,7 @@
 def require_app(folders = %w[services controllers])
   app_list = Array(folders).map { |folder| "app/#{folder}" }
   full_list = ['config', app_list].flatten.join(',')
-  Dir.glob("./#{full_list}/**/*.rb").each do |file|
+  Dir.glob("./{#{full_list}}/**/*.rb").each do |file|
     require file
   end
 end
